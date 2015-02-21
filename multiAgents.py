31b17d3d147d8b1a90708daef3c603487cef5490
Tianyun Sun <jiushiwosty@berkeley.edu>
2015-02-20 21:19:15 -0800

    Revert "success"
    This reverts commit f47587f1136a99dc792839097a37473cf6421e05.

--- multiAgents.py
+++ multiAgents.py
@@ -14,8 +14,6 @@
 
 from util import manhattanDistance
 from game import Directions
-from game import Agent
-from game import Actions
 import random, util
 
 from game import Agent
@@ -315,7 +313,6 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
             
         # bestaction = possibleaction
         return (average, None)
-
 def betterEvaluationFunction(currentGameState):
     """
       Your extreme ghost-hunting, pellet-nabbing, food-gobbling, unstoppable
@@ -330,13 +327,17 @@ def betterEvaluationFunction(currentGameState):
       linear combination ...
     """
     "*** YOUR CODE HERE ***"
-    BIGNUM = 99999
+    BIGNUM = float("inf")
     pacPos = currentGameState.getPacmanPosition()
     foodPos = currentGameState.getFood()
     ghostStates = currentGameState.getGhostStates() # in each state, show ghost scared or not
     # scaredTimes = [ghostState.scaredTimer for ghostState in ghostStates] #how much scared time they have left
     capPos = currentGameState.getCapsules()
     wallPos = currentGameState.getWalls()
+    if currentGameState.isLose():
+      return -BIGNUM
+    if currentGameState.isWin():
+      return BIGNUM
 
     numberFoodLeft = len(foodPos.asList())
     numberCapLeft = len(capPos)
@@ -344,17 +345,11 @@ def betterEvaluationFunction(currentGameState):
     score = 0
 
     #distance to closeset food
-    s1Food = BIGNUM;
-    s2Food = BIGNUM;
-    s3Food = BIGNUM;
+    shortestFood = BIGNUM;
     for p in foodPos.asList():
       d = util.manhattanDistance( pacPos, p)
-      if numberFoodLeft < 10:
-        d = getRealDis(currentGameState, p)
-      if d < s1Food:
-        s3Food = s2Food
-        s2Food = s1Food
-        s1Food = d
+      if d < shortestFood:
+        shortestFood = d
 
     #distance to closest cap
     shortestCap= BIGNUM;
@@ -368,183 +363,19 @@ def betterEvaluationFunction(currentGameState):
       p = g.getPosition()
       d = util.manhattanDistance( pacPos, p)
       if g.scaredTimer > 0: #scared 
-        score -= 4 * d 
+        score -= 100*d
       else:
         score = score + d
 
-    score = score - 100* numberFoodLeft - 300 * numberCapLeft - 2 * s1Food
-
-
+    score = score - 100* numberFoodLeft - 300 * numberCapLeft - 2 * shortestFood 
     if currentGameState.isLose():
-      score = -BIGNUM + currentGameState.getScore()
+      score = -BIGNUM
     if currentGameState.isWin():
-      score = BIGNUM + currentGameState.getScore()
+      score = BIGNUM
 
-    #print("here, ",pacPos, score, numberFoodLeft,"cap left, ", numberCapLeft)
+    #print("here, ",pacPos, score, numberFoodLeft)
     return score 
 
 # Abbreviation
 better = betterEvaluationFunction
 
-def getRealDis(gameState,p):
-  prob = PositionSearchProblem(gameState, p)
-  moves = breadthFirstSearch(prob)
-  #print(len(moves))
-  return len(moves)
-
-class SearchProblem:
-    """
-    This class outlines the structure of a search problem, but doesn't implement
-    any of the methods (in object-oriented terminology: an abstract class).
-
-    You do not need to change anything in this class, ever.
-    """
-
-    def getStartState(self):
-        """
-        Returns the start state for the search problem.
-        """
-        util.raiseNotDefined()
-
-    def isGoalState(self, state):
-        """
-          state: Search state
-
-        Returns True if and only if the state is a valid goal state.
-        """
-        util.raiseNotDefined()
-
-    def getSuccessors(self, state):
-        """
-          state: Search state
-
-        For a given state, this should return a list of triples, (successor,
-        action, stepCost), where 'successor' is a successor to the current
-        state, 'action' is the action required to get there, and 'stepCost' is
-        the incremental cost of expanding to that successor.
-        """
-        util.raiseNotDefined()
-
-    def getCostOfActions(self, actions):
-        """
-         actions: A list of actions to take
-
-        This method returns the total cost of a particular sequence of actions.
-        The sequence must be composed of legal moves.
-        """
-        util.raiseNotDefined()
-
-def breadthFirstSearch(problem):
-    """Search the shallowest nodes in the search tree first."""
-    "*** YOUR CODE HERE ***"
-    closed = set()
-    fringe = util.Queue()
-    startstate = [(problem.getStartState(), None, 0), []]
-    fringe.push(startstate)
-    while(True): 
-        if fringe.isEmpty():
-            return False
-        node = fringe.pop()
-        if problem.isGoalState(node[0][0]):
-            return node[1] 
-        if  node[0][0]  not in closed:
-            closed.add(node[0][0]) 
-            for triple in problem.getSuccessors(node[0][0]):
-                prevlst = node[1]
-                s = prevlst[:]
-                s.append(triple[1])
-                currstate = [triple, s]
-                fringe.push(currstate)
-
-class PositionSearchProblem(SearchProblem):
-    """
-    A search problem defines the state space, start state, goal test, successor
-    function and cost function.  This search problem can be used to find paths
-    to a particular point on the pacman board.
-
-    The state space consists of (x,y) positions in a pacman game.
-
-    Note: this search problem is fully specified; you should NOT change it.
-    """
-
-    def __init__(self, gameState, goal, costFn = lambda x: 1, start=None, warn=False, visualize=False):
-        """
-        Stores the start and goal.
-
-        gameState: A GameState object (pacman.py)
-        costFn: A function from a search state (tuple) to a non-negative number
-        goal: A position in the gameState
-        """
-        self.walls = gameState.getWalls()
-        self.startState = gameState.getPacmanPosition()
-        if start != None: self.startState = start
-        self.goal = goal
-        self.costFn = costFn
-        self.visualize = visualize
-        if warn and (gameState.getNumFood() != 1 or not gameState.hasFood(*goal)):
-            print 'Warning: this does not look like a regular search maze'
-
-        # For display purposes
-        self._visited, self._visitedlist, self._expanded = {}, [], 0 # DO NOT CHANGE
-
-    def getStartState(self):
-        return self.startState
-
-    def isGoalState(self, state):
-        isGoal = state == self.goal
-
-        # For display purposes only
-        if isGoal and self.visualize:
-            self._visitedlist.append(state)
-            import __main__
-            if '_display' in dir(__main__):
-                if 'drawExpandedCells' in dir(__main__._display): #@UndefinedVariable
-                    __main__._display.drawExpandedCells(self._visitedlist) #@UndefinedVariable
-
-        return isGoal
-
-    def getSuccessors(self, state):
-        """
-        Returns successor states, the actions they require, and a cost of 1.
-
-         As noted in search.py:
-             For a given state, this should return a list of triples,
-         (successor, action, stepCost), where 'successor' is a
-         successor to the current state, 'action' is the action
-         required to get there, and 'stepCost' is the incremental
-         cost of expanding to that successor
-        """
-
-        successors = []
-        for action in [Directions.NORTH, Directions.SOUTH, Directions.EAST, Directions.WEST]:
-            x,y = state
-            dx, dy = Actions.directionToVector(action)
-            nextx, nexty = int(x + dx), int(y + dy)
-            if not self.walls[nextx][nexty]:
-                nextState = (nextx, nexty)
-                cost = self.costFn(nextState)
-                successors.append( ( nextState, action, cost) )
-
-        # Bookkeeping for display purposes
-        self._expanded += 1 # DO NOT CHANGE
-        if state not in self._visited:
-            self._visited[state] = True
-            self._visitedlist.append(state)
-
-        return successors
-
-    def getCostOfActions(self, actions):
-        """
-        Returns the cost of a particular sequence of actions. If those actions
-        include an illegal move, return 999999.
-        """
-        if actions == None: return 999999
-        x,y= self.getStartState()
-        cost = 0
-        for action in actions:
-            # Check figure out the next state and see whether its' legal
-            dx, dy = Actions.directionToVector(action)
-            x, y = int(x + dx), int(y + dy)
-            if self.walls[x][y]: return 999999
-            cost += self.costFn((x,y))
-        return cost
